7d8d2c7082e1c4512c702794440a531eff372200
mustafagumuss <mustafa.gumuus0@gmail.com>
2026-01-27 20:45:26 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
-  basePath: "",
+  basePath: "mustafagumuss.github.io",
   images: {
     unoptimized: true,
   },
